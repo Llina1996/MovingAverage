b0129c8491dd762fb7d0fd63aacd2c4dc0a86e9e
Llina1996 <linasafyanowa@yandex.ru>
2015-10-27 21:27:16 +0300

    Main changes

---
+++ pres/MainWindow.xaml.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+
+namespace pres
+{
+    using OxyPlot;
+
+    public class MainWindowModel
+    {
+        PlotModel _model = null;
+
+        OxyPlot.Axes.DateTimeAxis _dateAxis = null;
+        public OxyPlot.Axes.DateTimeAxis DateAxis
+        {
+            get
+            {
+                if (_dateAxis == null)
+                {
+                    _dateAxis = new OxyPlot.Axes.DateTimeAxis
+                    {
+                        Position=OxyPlot.Axes.AxisPosition.Bottom
+                        , StringFormat = "dd/MM/yyyy"
+                        , Title = "Date"
+                       // , Minimum = OxyPlot.Axes.DateTimeAxis.ToDouble(new DateTime(2013, 1, 1))
+                      //  , Maximum = OxyPlot.Axes.DateTimeAxis.ToDouble(new DateTime(2014, 1, 1))
+                      //  , IntervalLength = 75
+                      
+                        , MinorIntervalType = OxyPlot.Axes.DateTimeIntervalType.Months
+                        , IntervalType = OxyPlot.Axes.DateTimeIntervalType.Months
+                        , MajorGridlineStyle = LineStyle.Solid
+                        , MinorGridlineStyle = LineStyle.None
+                        , Angle = -90
+                    };
+                    _dateAxis.ExtraGridlineStyle = LineStyle.None;
+                    _dateAxis.MinorTickSize = 0;
+                }
+                return _dateAxis;
+            }
+        }
+
+        OxyPlot.Series.CandleStickSeries _series = null;
+        public OxyPlot.Series.CandleStickSeries CandleSeries
+        {
+            get
+            {
+                if (_series == null)
+                {
+                    _series = new OxyPlot.Series.CandleStickSeries();
+                    //_series.CandleWidth = OxyPlot.Axes.DateTimeAxis.ToDouble(new DateTime(2013, 3, 20)) - OxyPlot.Axes.DateTimeAxis.ToDouble(new DateTime(2013, 3, 5));
+                    //_series.Items.Add(new OxyPlot.Series.HighLowItem(
+                    //    OxyPlot.Axes.DateTimeAxis.ToDouble(new DateTime(2013, 4, 15))
+                    //    , 20, 15, 17, 19));
+                }
+                return _series;
+            }
+        }
+         OxyPlot.Series.LineSeries _average = null;
+         public OxyPlot.Series.LineSeries Average
+         {
+             get
+             {
+                 if (_average == null)
+                 {
+                     _average = new OxyPlot.Series.LineSeries();
+                     //_average.CandleWidth = OxyPlot.Axes.DateTimeAxis.ToDouble(new DateTime(2013, 3, 20)) - OxyPlot.Axes.DateTimeAxis.ToDouble(new DateTime(2013, 3, 5));
+                     //_average.Items.Add(new OxyPlot.Series.HighLowItem(
+                     //    OxyPlot.Axes.DateTimeAxis.ToDouble(new DateTime(2013, 4, 15))
+                     //    , 20, 15, 17, 19));
+                 }
+                 return _average;
+             }
+         }
+
+        public PlotModel DataPlot
+        {
+            get 
+            {
+                if (_model == null)
+                {
+                    _model = new PlotModel();
+                    _model.Axes.Add(DateAxis);
+                    _model.Axes.Add(PriceAxis);
+                    _model.Series.Add(CandleSeries);
+                }
+                return _model;
+            }
+        }
+
+        OxyPlot.Axes.LinearAxis _priceAxis=null;
+        public OxyPlot.Axes.LinearAxis PriceAxis
+        {
+            get
+            {
+                if (_priceAxis == null)
+                {
+                    _priceAxis = new OxyPlot.Axes.LinearAxis
+                    {
+                        Position = OxyPlot.Axes.AxisPosition.Left,
+           
+                    };
+                }
+                return _priceAxis;
+            }
+        }
+
+        public void ShowCandleSeries(pres.TimeFrame timeFrame,IEnumerable<StockIntervalData> candelSeries) 
+        {
+           CandleSeries.Items.Clear();
+           IEnumerable<OxyPlot.Series.HighLowItem> hl = candelSeries.Select(
+               x => new OxyPlot.Series.HighLowItem(OxyPlot.Axes.DateTimeAxis.ToDouble(x.date),
+               (double)x.high,(double)x.low,(double)x.open,(double)x.close)
+           );
+           CandleSeries.Items.AddRange(hl);
+           if (timeFrame == TimeFrame.Month)
+           {
+               DateAxis.IntervalType = OxyPlot.Axes.DateTimeIntervalType.Months;
+               DateAxis.MinorIntervalType = OxyPlot.Axes.DateTimeIntervalType.Months;
+           }
+           else if (timeFrame == TimeFrame.Week)
+           {
+               DateAxis.IntervalType = OxyPlot.Axes.DateTimeIntervalType.Weeks;
+               DateAxis.MinorIntervalType = OxyPlot.Axes.DateTimeIntervalType.Weeks;
+           }
+           else if (timeFrame == TimeFrame.Day)
+           {
+               DateAxis.IntervalType = OxyPlot.Axes.DateTimeIntervalType.Days;
+               DateAxis.MinorIntervalType = OxyPlot.Axes.DateTimeIntervalType.Days;
+           }
+
+        }
+        public void ShowMovingAverage(pres.TimeFrame timeFrame, IEnumerable<StockIntervalData> candelSeries)
+        {
+         
+        }
+    }
+    
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Worker w = new Worker();
+            try
+            {
+                ((MainWindowModel)DataContext).ShowCandleSeries(TimeFrame.Week, w.GetStockData(TimeFrame.Week, "AAPL", new DateTime(2013, 1, 1), new DateTime(2014, 1, 1)));
+            }
+            catch (WebException webEx)
+            {
+                Console.WriteLine(webEx.ToString());
+            }
+
+        }
+    }
+}
---
+++ pres/Worker.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Globalization;
+
+namespace pres
+{
+
+
+
+    public enum TimeFrame
+    {
+        Day, Week, Month
+
+    }
+
+    public struct StockIntervalData
+    {
+        public DateTime date;
+        public decimal open;
+        public decimal close;
+        public decimal low;
+        public decimal high;
+        public decimal volume;
+    }
+    class Worker
+    {
+        public byte[] Download(string address)
+        {
+            // Download the data to a buffer.
+            WebClient client = new WebClient();
+
+            Byte[] pageData = client.DownloadData(address);
+            /*
+            string pageHtml = Encoding.ASCII.GetString(pageData);
+            Console.WriteLine(pageHtml);
+            */
+            return pageData;
+        }
+       public struct Average{
+           public DateTime date;
+           public decimal valueAverage;
+        }
+
+        public decimal calculateAveage (List<Average> c,int start,int end )
+        {
+            decimal res = 0;
+            for (int i = start; i <= end; ++i)
+            {
+                res += c[i].valueAverage;
+            }
+            return res / (end - start + 1);
+        }
+        public List<Average> movingAverage(IList<StockIntervalData> intervals, int N){
+        List<Average> result  = new List<Average>();
+            for (int i = 0; i < intervals.Count; ++i)
+            {
+                
+                decimal c = (intervals[i].open + intervals[i].close) / 2;
+                Average res = new Average { date = intervals[i].date , valueAverage = c };
+                result.Add(res);
+            }
+            for (int i =N-1;i >=0; --i ){
+                
+                if (( i+1)<N) {
+                 Average a= result[i] ;//.va= calculateAveage(result,0,i);
+                    a.valueAverage=calculateAveage(result,0,i);
+                    result[i]=a;
+                }
+                else {
+                    Average a = result[i];
+                  a.valueAverage=calculateAveage(result,i-N+1,i);
+                  result[i] = a;
+                }
+            }
+            return result;
+
+        }
+
+        public List<StockIntervalData> ConvertTimeFrame(List<StockIntervalData> intervals ,TimeFrame timeFrame )
+        {
+            if (timeFrame == TimeFrame.Day)
+            {
+                return intervals;
+            }
+            else if (timeFrame == TimeFrame.Week)
+            {
+                bool first = true;
+                StockIntervalData weekData = new StockIntervalData();
+                List<StockIntervalData> weeksIntervals = new List<StockIntervalData>();
+                foreach (StockIntervalData dayData in intervals)
+                {
+                    if(first || (dayData.date - weekData.date).Days>=7 )
+                    {
+                        if (!first)
+                            weeksIntervals.Add(weekData);
+                        weekData = dayData;
+                        weekData.date.AddDays(-(int)(weekData.date.DayOfWeek));   /// откатить  дату на начало недели (воскресенье)
+                        first = false;
+                    }
+                    else 
+                    {
+                        weekData.close = dayData.close;
+                        if (dayData.low < weekData.low)
+                            weekData.low = dayData.low;
+                        if (dayData.high > weekData.high)
+                            weekData.high = dayData.high;
+                        weekData.volume += dayData.volume;
+                    }
+
+                }
+                if (!first)
+                {
+                    weeksIntervals.Add(weekData);
+                }
+                return weeksIntervals;
+            }
+            else if (timeFrame == TimeFrame.Month)
+            {
+                bool first = true;
+                StockIntervalData monthData = new StockIntervalData();
+                List<StockIntervalData> monthsIntervals = new List<StockIntervalData>();
+                foreach (StockIntervalData dayData in intervals)
+                {
+                    if (first || monthData.date.Month != dayData.date.Month || monthData.date.Year != dayData.date.Year)
+                    {
+                        if (!first)
+                            monthsIntervals.Add(monthData);
+                        monthData = dayData;
+                        monthData.date = new DateTime(dayData.date.Year, dayData.date.Month, 1);
+                        first = false;
+                    }
+                    else
+                    {
+                        monthData.close = dayData.close;
+                        if (dayData.low < monthData.low)
+                            monthData.low = dayData.low;
+                        if (dayData.high > monthData.high)
+                            monthData.high = dayData.high;
+                        monthData.volume += dayData.volume;
+                    }
+                }
+                if (!first)
+                {
+                    monthsIntervals.Add(monthData);
+                }
+                return monthsIntervals;
+            }
+            else
+            {
+                // TODO: years
+                return intervals;
+            }
+        }
+
+
+        public List<StockIntervalData> ParseStockData(byte[] data)
+        {
+            List<StockIntervalData> result = new List<StockIntervalData>();
+            StreamReader reader = new StreamReader(new MemoryStream(data), Encoding.ASCII);
+            reader.ReadLine();
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                try
+                {
+                    StockIntervalData d = new StockIntervalData();
+                    CultureInfo provider = CultureInfo.InvariantCulture;
+                    string[] tokens = line.Split(new char[] { ',' });
+                    d.date = DateTime.ParseExact(tokens[0], "yyyy-MM-dd", provider);
+                    d.open = System.Decimal.Parse(tokens[1], provider);
+                    d.high = System.Convert.ToDecimal(tokens[2], provider);
+                    d.low = System.Convert.ToDecimal(tokens[3], provider);
+                    d.close = System.Convert.ToDecimal(tokens[4], provider);
+                    d.volume = System.Convert.ToDecimal(tokens[5], provider);
+                    result.Insert(0, d);
+                }
+                catch (FormatException fex)
+                {
+                    Console.WriteLine(fex.Message);
+                }
+            }
+            return result;
+        }
+
+        public List<StockIntervalData> GetStockData(TimeFrame timeframe, string stockId, DateTime from, DateTime to)
+        {
+            // http://real-chart.finance.yahoo.com/table.csv?s=CB&d=9&e=30&f=2014&g=m&a=8&b=7&c=2013&ignore=.csv
+            string url = "http://real-chart.finance.yahoo.com/table.csv?s=" + stockId
+                + "&d=" + (to.Month - 1) + "&e=" + to.Day + "&f=" + to.Year // date to
+                + "&a=" + (from.Month - 1) + "&b=" + (from.Day) + "&c=" + (from.Year)  //date from
+                + "&g=d&ignore=.csv";
+
+
+            byte[] data = Download(url);
+            List<StockIntervalData> intervals = ParseStockData(data);
+            intervals = ConvertTimeFrame(intervals, timeframe);
+            return intervals;
+        }
+
+    }
+}
